6c1fe81b9dd244dc695ff55ca5965b909b1373e7
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-11-15 15:12:58 +0100

    TagCloud improvements

--- js/tagCloud.js
+++ js/tagCloud.js
@@ -5,3 +5,27 @@ window.onload = function() {
         console.log("Something went wrong");
     }
 };
+
+
+var tagCloudData = [
+    {
+        "link": "Aleassa,_H.",
+        "text": "Aleassa, H."
+    },
+    {
+        "link": "Pearson,_J._M.",
+        "text": "Pearson, J. M."
+    },
+    {
+        "link": "Mcclurg,_S.",
+        "text": "Mcclurg, S."
+    }
+]
+
+function fillTagCloud(tags) {
+    for (var i = 0; i < tags.length; i++) {
+        var tag_text = "<a href=\"http://www.copyrightevidence.org/evidence-wiki/index.php/" + tags[i].link + "\" target=\"_blank\">" + tags[i].text + "</a>";
+        console.log(tag_text);
+        $('#foo_canvas').append(tag_text);
+    }
+};
